2d9d879716e458bc81e6efde7f1db0c4e160405d
SuJaL3511 <95642419+SUJAL123612@users.noreply.github.com>
2025-10-31 01:42:37 +0530

    Update page.tsx

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -8,7 +8,7 @@ export default function Page() {
         <div className="relative z-20">
           <Navbar />
         </div>
-        <img src="library.jpg" alt="library" className="w-full h-screen object-cover" />
+        <img src="/library.jpg" alt="library" className="w-full h-screen object-cover" />
         <div className="absolute inset-0 flex flex-col items-center justify-center text-center">
           <h1 className="text-white text-6xl font-bold text-shadow-lg-dark cursor-default">Welcome to The Online Library</h1>
         </div>
